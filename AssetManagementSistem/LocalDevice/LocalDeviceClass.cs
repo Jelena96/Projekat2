78907dc007646c1c5f9a10c4a6c08acbffbd7f6f
elena1237 <elenakevac@gmail.com>
2018-05-26 19:11:05 +0200

    F-ja rand za digital

--- AssetManagementSistem/LocalDevice/LocalDeviceClass.cs
+++ AssetManagementSistem/LocalDevice/LocalDeviceClass.cs
@@ -21,9 +21,8 @@ namespace LocalDevice
 
         public string DeviceType { get; set; }
 
-
-
         public string SendTo { get; set; }
+        public DeviceEnum ActualState { get; set; }
 
         public LocalDeviceClass()
         {
@@ -31,21 +30,24 @@ namespace LocalDevice
         }
 
         private static Random ran = new Random();
-        private static DeviceEnum GetType3()
+       /* private static DeviceEnum GetType3()
         {
             return (DeviceEnum)(ran.Next(0, 3));
 
-        }
-
-
-
+        }*/
         private static int GetType2()
         {
 
-            return ran.Next(0, 10);
+            return ran.Next(1, 10);
         }
+        private static int GetType1()
+        {
 
+            return ran.Next(0, 2);  //Ako zelimo broj 0 ili 1 onda ide random(0,2)
+        }
         //  public AnalogDeviceEnum AnalogDevice { get; set; }
+
+        private static List<string> aktivniKontroleri = new List<string>();
 
         public bool CreateXML(string p1)
         {
@@ -53,8 +55,6 @@ namespace LocalDevice
             bool u = false;
             using (XmlWriter writer = XmlWriter.Create(p1))
             {
-
-              
 
                     foreach (KeyValuePair<int, List<LocalDeviceClass>> item in Program.DeviceDic)
                     {
@@ -65,7 +65,7 @@ namespace LocalDevice
                             writer.WriteStartDocument();
                             writer.WriteStartElement("Devices");
 
-                        while (j != 3) {
+                        while (j!=3) {
 
                             foreach (LocalDeviceClass device in item.Value)
                             {
@@ -76,17 +76,19 @@ namespace LocalDevice
 
                                 //Enum.GetName(typeof(DeviceEnum), e);
 
-                                writer.WriteElementString("ID", device.LocalDeviceCode.ToString());
-                                if (device.DeviceType == "D")
-                                {
-                                    writer.WriteElementString("ActualValue", GetType3().ToString());
-                                }
                                 writer.WriteElementString("Type", device.DeviceType);
-                                writer.WriteElementString("TimeStamp", DateTime.Now.ToString());
+                                writer.WriteElementString("ID", device.LocalDeviceCode.ToString());
                                 writer.WriteElementString("SendTo", device.SendTo);
+                                writer.WriteElementString("ActualValue", device.ActualValue.ToString());
+                                writer.WriteElementString("ActualState", device.ActualState.ToString());
+                                writer.WriteElementString("TimeStamp", DateTime.Now.ToString());
+                                
                                 if (device.DeviceType == "A")
                                 {
-                                    writer.WriteElementString("AnalogMeasurment", GetType2().ToString());
+                                    writer.WriteElementString("Measurment", GetType2().ToString());
+                                } else if(device.DeviceType=="D")
+                                {
+                                    writer.WriteElementString("Measurment", GetType1().ToString());
                                 }
                                 writer.WriteEndElement();
 
@@ -117,11 +119,24 @@ namespace LocalDevice
                 }
                 else
                 {
-                  
-
-                    p = files[0].Split('\\');
-                    ime = p[4].Substring(0, p[4].Length - 4);
-                    Console.WriteLine("Postoje fajlovi{0} ", ime);
+                    Console.WriteLine("***Postoje kontroleri:***");
+                    foreach (var file in files)
+                    {
+                        p = file.Split('\\');
+                        ime = p[4].Substring(0, p[4].Length - 4);
+                        aktivniKontroleri.Add(ime);
+                       
+                        foreach (var item in aktivniKontroleri)
+                        {
+                            Console.WriteLine("{0}", item);
+                        }
+                        aktivniKontroleri.Remove(ime);
+                    }
+                    
+                    
+                    
+                    
+                    
 
                 }
                 return u;
