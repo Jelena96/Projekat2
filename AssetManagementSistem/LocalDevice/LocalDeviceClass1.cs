12e3665685f0558c8c92ff28cfa13e0ebed767b3
Jelena96 <37114083+Jelena96@users.noreply.github.com>
2018-06-05 22:13:29 +0200

    Promena stanja uredjaja

--- AssetManagementSistem/LocalDevice/LocalDeviceClass1.cs
+++ AssetManagementSistem/LocalDevice/LocalDeviceClass1.cs
@@ -15,160 +15,12 @@ namespace LocalDevice
     [DataContract]
     public class LocalDeviceClass1 
     {
-        //[DataMember]
-        //public int localDeviceCode;
-        //public int LocalDeviceCode {
-
-        //    get {
-
-        //        return localDeviceCode;
-
-        //    }
-        //    set {
-
-        //        localDeviceCode = value;
-        //    }
-        //}
-        //[DataMember]
-        //public int idControler;
-
-        //public int IdControler
-        //{
-
-        //    get
-        //    {
-
-        //        return idControler;
-
-        //    }
-        //    set
-        //    {
-
-        //        idControler = value;
-        //    }
-        //}
-        //[DataMember]
-        //public DateTime timestamp;
-
-        //public DateTime TimeStamp
-        //{
-
-        //    get
-        //    {
-
-        //        return timestamp;
-
-        //    }
-        //    set
-        //    {
-
-        //        timestamp = value;
-        //    }
-        //}
-        //[DataMember]
-        //public int analogActualValue;
-
-        //public int AnalogActualValue
-        //{
-
-        //    get
-        //    {
-
-        //        return analogActualValue;
-
-        //    }
-        //    set
-        //    {
-
-        //        analogActualValue = value;
-        //    }
-        //}
-        //[DataMember]
-        //public DeviceEnum actualValue;
-        //public DeviceEnum ActualValue
-        //{
-
-        //    get
-        //    {
-
-        //        return actualValue;
-
-        //    }
-        //    set
-        //    {
-
-        //        actualValue = value;
-        //    }
-        //}
-        //[DataMember]
-
-        //public string deviceType;
-        //public string DeviceType
-        //{
-
-        //    get
-        //    {
-
-        //        return deviceType;
-
-        //    }
-        //    set
-        //    {
-
-        //        deviceType = value;
-        //    }
-        //}
-        //[DataMember]
-
-        //public string sendTo;
-        //public string SendTo
-        //{
-
-        //    get
-        //    {
-
-        //        return sendTo;
-
-        //    }
-        //    set
-        //    {
-
-        //        sendTo = value;
-        //    }
-        //}
-        //[DataMember]
-        //public DeviceEnum actualState;
-        //public DeviceEnum ActualState
-        //{
-
-        //    get
-        //    {
-
-        //        return actualState;
-
-        //    }
-        //    set
-        //    {
-
-        //        actualState = value;
-        //    }
-        //}
-
-        
-
-
-
+       
+        public static int promene;
         public LocalDeviceClass1() { }
 
-
-
-
         private static Random ran = new Random();
-        /* private static DeviceEnum GetType3()
-         {
-             return (DeviceEnum)(ran.Next(0, 3));
-
-         }*/
+        
         private static int GetType2()
         {
 
@@ -179,10 +31,16 @@ namespace LocalDevice
 
             return ran.Next(0, 2);  //Ako zelimo broj 0 ili 1 onda ide random(0,2)
         }
-        //  public AnalogDeviceEnum AnalogDevice { get; set; }
+        
 
-      
 
+        public void PromenaStanja(int id,int idk,string type, LocalDeviceClass l,string promena) {
+
+            
+            
+            
+
+        }
         
 
 
